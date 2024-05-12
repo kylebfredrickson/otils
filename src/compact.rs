2b4bd1fc150dae1191c61c43b2757d6d18a0bfb0
Kyle Fredrickson <kyfredri@ucsc.edu>
2024-05-12 13:26:31 -0700

    Modified api for obliviousness.

--- src/compact.rs
+++ src/compact.rs
@@ -12,9 +12,9 @@ pub fn ofilter<T: ObliviousOps + marker::Send, F>(
     threads: u8,
 ) -> Vec<T>
 where
-    F: Fn(&T) -> bool,
+    F: Fn(&T) -> i8,
 {
-    let bits: Vec<usize> = data.iter().map(f).map(|b| if b { 1 } else { 0 }).collect();
+    let bits: Vec<usize> = data.iter().map(|x| f(x).try_into().unwrap()).collect();
     parallel_or_compact(&mut data[..], &bits[..], threads);
     data.truncate(num_matches);
     data
@@ -29,13 +29,14 @@ mod tests {
         macro_rules! test_filter {
             ($v: expr, $f: expr, $t: ty) => {
                 let data: Vec<$t> = $v.into_iter().collect();
-                let real: Vec<$t> = $v.into_iter().filter($f).collect();
+                let real: Vec<$t> = $v.into_iter().filter(|x| ($f(x) == 1) as bool).collect();
                 let test = ofilter(data, $f, real.len(), 2);
                 assert_eq!(test, real);
             };
         }
 
-        test_filter!((1..11), |x| x % 2 == 0, i32);
-        test_filter!((1..11), |x| x % 3 == 0, i32);
+        test_filter!((1..11), |x| i32::oequal(x % 2, 0), i32);
+        test_filter!((1..11), |x| i32::oequal(x % 3, 0), i32);
+        test_filter!((1..101), |x| i32::oequal(x % 7, 0), i32);
     }
 }
